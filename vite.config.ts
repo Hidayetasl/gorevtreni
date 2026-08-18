e968580464770a197bcd3c883bd73c5d1730b569
Hidayet Aslan <hidayetaslan@gmail.com>
2026-08-18 23:18:15 +0300

    Repo adresini gorevtreni olarak güncelle (gorev-treni -> gorevtreni)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_PAGES === 'true' ? '/gorev-treni/' : '/',
+    base: process.env.GITHUB_PAGES === 'true' ? '/gorevtreni/' : '/',
     plugins: [
       react(),
       tailwindcss(),
